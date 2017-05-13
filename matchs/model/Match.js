01216416e5bbce9f6031533bfbb77693a6113509
Benjalal <benj.balaska@gmail.com>
2017-05-13 18:43:42 +0200

    [FINAL VERSION 13.05.17]

--- matchs/model/Match.js
+++ matchs/model/Match.js
@@ -12,15 +12,9 @@ const matchModel = new Schema({
   scoreDom: { type: Number, min:0, defaultsTo:0},
   scoreExt: { type: Number, min:0, defaultsTo:0 },
   bets: [{type: Schema.Types.ObjectId, ref: 'Bet', unique:true}],
-  //bets: {collection: 'Bet'  },
-  //vainqueur: { type: String, required: false }
+  
 });
 
-/*matchModel.post('remove', function(next) {
-    // 'this' is the client being removed. Provide callbacks here if you want
-    // to be notified of the calls' result.
-    Bet.remove({client_id: this._id}).exec();
-    next();
-});*/
+
 
 module.exports = mongoose.model('Match', matchModel);
